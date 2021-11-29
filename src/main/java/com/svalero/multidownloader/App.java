9cf6200234f829450077829a9f83bedcf4e50937
David de San Lázaro <david.dslazaro@gmail.com>
2021-11-29 17:25:14 +0100

    Proyecto base

---
+++ src/main/java/com/svalero/multidownloader/App.java
@@ -0,0 +1,39 @@
+package com.svalero.multidownloader;
+
+import com.svalero.multidownloader.util.R;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(R.getUI("multidownload.fxml"));
+        loader.setController(new AppController());
+        ScrollPane vbox = loader.load();
+
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+        stage.setTitle("Downloader");
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
